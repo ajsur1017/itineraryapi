eb7458f9a4aadf0fe31b8f04168dd9b09f4b5dfb
Anthony <ajsur1017@gmail.com>
2022-01-06 13:29:29 -0500

    added backedn auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   resources :wednesdays
   resources :tuesdays
   resources :mondays
+
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: "test", password: "test", age: "30")
+monday = Monday.create(name: "Leisure Time", details: "Play some games or watch some anime", time: "10:00 AM" )
+monday2 = Monday.create(name: "Exercise", details: "Head to the gym and get swol", time: "12:00 PM")
+monday3 = Monday.create(name: "Movie Date", details: "Check out the new Spiderman with the Wife", time: "3:00 PM")
+monday4 = Monday.create(name: "Dinner Date", details: "Noms", time: "6:00 PM")
+monday5 = Monday.create(name: "Gaming with the boys", details: "The Boys!!!!", time: "8:30 PM")
